26f9f19bb98f8952b34be6873f33d86da651be34
zaidiawan <zahidraza.nust@gmail.com>
2024-04-07 20:36:37 +0200

    modified:   server3.py

--- server3.py
+++ server3.py
@@ -1,29 +1,35 @@
 import asyncio
 import json
-import time
 import websockets
 from confluent_kafka import Producer
 from cassandra.cluster import Cluster
 from cassandra.policies import ExponentialReconnectionPolicy, RoundRobinPolicy, RetryPolicy
 
-
 async def subscribe_to_agg_trade_feed(symbol, producer, session):
     uri = f"wss://fstream.binance.com/ws/{symbol.lower()}@aggTrade"
     async with websockets.connect(uri) as websocket:
         while True:
-            message = await websocket.recv()
-            trade_data = json.loads(message)
-            await publish_via_kafka(symbol, trade_data, producer)
-            await store_in_cassadra(symbol, trade_data, session)
-
+            try:
+                message = await asyncio.wait_for(websocket.recv(), timeout=5.0)  #timeout
+                trade_data = json.loads(message)
+                await asyncio.gather(
+                    publish_via_kafka(symbol, trade_data, producer),
+                    store_in_cassandra(symbol, trade_data, session)
+                )
+            except asyncio.TimeoutError:
+                print("timeout has been occured...")
+                continue
+            except websockets.ConnectionClosedError:
+                print("Websockets connection has been closed")
+                await asyncio.sleep(6)  
+                break  
 
 async def publish_via_kafka(symbol, trade_data, producer):
     topic = f"{symbol}_agg_trades"
     producer.produce(topic, json.dumps(trade_data))
     producer.flush()
 
-
-async def store_in_cassadra(symbol, trade_data, session):
+async def store_in_cassandra(symbol, trade_data, session):
     try:
         table_name = symbol + '_Node'
         session.execute(f"""
@@ -48,52 +54,40 @@ async def store_in_cassadra(symbol, trade_data, session):
                 {trade_data['p']}, {trade_data['q']}, {int(trade_data['f'])}, '{trade_data['l']}', {trade_data['T']}, {trade_data['m']})
         """
         session.execute(insert_query)
-        #print("data successfully entered into Cassandra")
-    
+
     except Exception as e:
-        print("Failed to interact with Cassandra:", e)
+        print("Failed to store in Cassandra:", e)
 
-
-async def main(symbols):
-    retry_policy = RetryPolicy()
-    reconnection_policy = ExponentialReconnectionPolicy(1.0, 60.0)
-    load_balancing_policy = RoundRobinPolicy()
-
-    # Cassandra initialization may take some time, so sleep for 20 seconds
-    await asyncio.sleep(20)
-    while True:
-        try:
-            cluster = Cluster(['cassandra'])
-            cluster.default_retry_policy = RetryPolicy()
-            cluster.reconnection_policy = reconnection_policy
-
-            create_keyspace_query = f"CREATE KEYSPACE IF NOT EXISTS {'binance'}" \
-                                    f" WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1  }}"
-
-            session = cluster.connect()
-            session.execute(create_keyspace_query)
-
-            print("Key space created successfully!.......")
-
-
-            session = cluster.connect('binance')
-            session.load_balancing_policy = load_balancing_policy
-
-            producer_conf = {'bootstrap.servers': 'kafka'}
-            producer = Producer(producer_conf)
-
-            tasks = [subscribe_to_agg_trade_feed(symbol, producer, session) for symbol in symbols]
-            await asyncio.gather(*tasks)
-
-            break
-
-        except Exception as e:
-            print(f"Failed to connect to Cassandra: {e}")
-            print("Retrying in 5 seconds...")
-            await asyncio.sleep(5)
+async def main(symbols, producer, session):
+    tasks = [subscribe_to_agg_trade_feed(symbol, producer, session) for symbol in symbols]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT']
-    print("Waiting for Cassandra to start....")
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(symbols))
+    try:
+        cluster = Cluster(['cassandra'])
+        cluster.default_retry_policy = RetryPolicy()
+        cluster.reconnection_policy = ExponentialReconnectionPolicy(1.0, 60.0)
+
+        create_keyspace_query = f"CREATE KEYSPACE IF NOT EXISTS {'binance'}" \
+                                f" WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1  }}"
+
+        session = cluster.connect()
+        session.execute(create_keyspace_query)
+        session = cluster.connect('binance')
+
+        print("connection to Cassandra has been established..")
+        
+        session.load_balancing_policy = RoundRobinPolicy()
+
+        producer_conf = {'bootstrap.servers': 'kafka'}
+        producer = Producer(producer_conf)
+
+        print("Now going to continuously publish and store")
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(symbols, producer, session))
+
+    except Exception as e:
+
+        print(f"Failed to connect to Cassandra: {e}")
